a274d15d79f5702afb19687b6ad1afb1429ebebb
HansTtito <kvttitos@gmail.com>
2024-02-23 18:42:03 -0300

    general functions updated

--- general_functions.py
+++ general_functions.py
@@ -1,11 +1,10 @@
 
 
 def read_files_from_bucket(s3_client, bucket_name, object_key, index_bucket = 'Body'):
-    try: 
-        response = s3_client.get_object(Bucket = bucket_name, Key = object_key)
-        output = response[index_bucket].read().decode('utf-8', errors='replace')
-    except Exception as e:
-        print(f"Error al leer el archivo CSV en S3: {e}")
+    
+    response = s3_client.get_object(Bucket = bucket_name, Key = object_key)
+    output = response[index_bucket].read().decode('utf-8', errors='replace')
+        
     return output
 
 def guardar_archivo_en_s3(s3_client, content, s3_key, s3_bucket_name):
@@ -15,3 +14,10 @@ def guardar_archivo_en_s3(s3_client, content, s3_key, s3_bucket_name):
 
     except Exception as e:
         print(f"Error al guardar el archivo CSV en S3: {e}")
+        
+def eliminar_archivo_en_s3(s3_client, bucket_name, key):
+    try:
+        s3_client.delete_object(Bucket=bucket_name, Key=key)
+        print(f"Archivo eliminado en S3: {key}")
+    except Exception as e:
+        print(f"Error al eliminar el archivo en S3: {e}")       
